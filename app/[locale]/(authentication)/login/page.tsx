3cf82211038cafc78c4b4c6f9b8a659ad49fa83b
Isreal Oparanti <isrealopa@gmail.com>
2024-10-09 16:53:54 +0100

    new commit

--- app/[locale]/(authentication)/login/page.tsx
+++ app/[locale]/(authentication)/login/page.tsx
@@ -25,6 +25,7 @@ export default function Login() {
             appleText={t("appleLogin")}
           />
           <Divider /> */}
+          
           <LoginForm
             emailPlaceholder={t("email")}
             passwordPlaceholder={t("loginPassword")}
